5d5e8985315aa31fda37c89fecb4464429888ebe
Dianze Liu <114109108+Jerryliu3547@users.noreply.github.com>
2024-06-24 22:03:48 +0000

    styled the input sheets

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,33 @@
-let shareList = ["Apple", "Microsoft", "Tesla"];
-
+let shareList = [{
+    stockId: "AAPL",
+    stockFullName:"Apple",
+    shareAmount: 1,
+    pricePerShare: 207
+}, 
+{
+    stockId:"MSFT",
+    stockFullName:"Microsoft",
+    shareAmount: 1,
+    pricePerShare: 450
+}, 
+{   
+    stockId: "TSLA",
+    stockFullName:"Tesla",
+    shareAmount: 1,
+    pricePerShare: 184
+}]
 
 function updatingStockList() {
     let shareListHTML = '';
 
     for(let i = 0; i < shareList.length; i++) { 
         const shareListItem = shareList[i];
-        const html = `<p>${shareListItem}</p>`;
+        const html = `
+        <div>${shareListItem.stockId}</div> 
+        <div>${shareListItem.stockFullName}</div> 
+        <div>${shareListItem.shareAmount}</div> 
+        <div>${shareListItem.pricePerShare}</div> 
+        <button onclick = "shareList.splice(${i},1);updatingStockList();">Delete</button>`;
         shareListHTML += html;
     }
 
@@ -15,12 +36,17 @@ function updatingStockList() {
 };
 updatingStockList();
 
+// add stock to current list
 document.querySelector('.js-add-portfolio-button').addEventListener('click', () => {
     const stockFullName = document.querySelector('.js-stock-full-name').value;
-    shareList.push(stockFullName);
+    shareList.stockFullName.push(stockFullName);
     updatingStockList();
     // Log the shareList each time a new stock is added
 });
+
+
+
+
 
 
 
