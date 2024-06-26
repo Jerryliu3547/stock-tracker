4ff06e63afe9430f43f7fe40ed824631c98fc527
Dianze Liu <114109108+Jerryliu3547@users.noreply.github.com>
2024-06-26 03:45:01 +0000

    in java script add api to retrieve stock info

--- scripts/index.js
+++ scripts/index.js
@@ -62,3 +62,29 @@ document.querySelector('.js-add-portfolio-button').addEventListener('click', () 
     shareList.push(stockItem);
     updatingStockList();
 });
+
+const apiKey = 'optPy7m1I2kXjJRGA5pR4CX9J3OXDQN5';
+
+
+async function fetchStockPrice(date) {
+    const url = `https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/${date}?adjusted=true&apiKey=${apiKey}`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        if (data.results && data.results.length > 0) {
+            return data.results;
+        } else {
+            throw new Error('Failed to fetch stock price');
+        }
+    } catch (error) {
+        console.error('Error fetching stock price:', error);
+        return null;
+    }
+}
+
+// Example usage:
+fetchStockPrice('2023-01-09').then(data => {
+    if (data !== null) {
+        console.log('The latest aggregated stock data:', data);
+    }
+});
